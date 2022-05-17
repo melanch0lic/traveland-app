411a7352e0151d44255e4217b928d7512c85ebe9
melanch0lic <kudza76@mail.ru>
2022-05-18 02:03:16 +0300

    added animation to splash page

--- hackground_app/lib/pages/splash_page.dart
+++ hackground_app/lib/pages/splash_page.dart
@@ -3,11 +3,39 @@ import 'package:flutter/material.dart';
 import '../unities.dart';
 import '../pages/tabs_page.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage>
+    with SingleTickerProviderStateMixin {
+  AnimationController? earthController;
+  Animation<double>? rotationAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    earthController =
+        AnimationController(duration: const Duration(seconds: 4), vsync: this)
+          ..repeat();
+
+    rotationAnimation = Tween<double>(begin: 0, end: 1).animate(
+        CurvedAnimation(parent: earthController!, curve: Curves.linear));
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    earthController!.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     Future.delayed(
-        Duration(seconds: 2),
+        Duration(seconds: 4),
         () => Navigator.of(context)
             .push(MaterialPageRoute(builder: (context) => TabsScreen())));
     return Stack(
@@ -20,12 +48,15 @@ class SplashPage extends StatelessWidget {
             ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
           ),
         ),
-        const Align(
-          alignment: Alignment.center,
-          child: Icon(
-            Icons.public_outlined,
-            color: Colors.white,
-            size: 100,
+        RotationTransition(
+          turns: rotationAnimation!,
+          child: const Align(
+            alignment: Alignment.center,
+            child: Icon(
+              Icons.public,
+              color: Colors.white,
+              size: 100,
+            ),
           ),
         )
       ],
