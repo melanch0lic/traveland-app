1c9c864bf7fda5137c1d09bb438a76885eaca844
melanch0lic <kudza76@mail.ru>
2022-11-14 21:13:21 +0300

    search widget changes

--- hackground_app/lib/widgets/search_widget.dart
+++ hackground_app/lib/widgets/search_widget.dart
@@ -3,31 +3,40 @@ import 'package:flutter/material.dart';
 import '../app_localizations.dart';
 
 class SearchWidget extends StatelessWidget {
+  const SearchWidget({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    return Container(
       padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
-      child: SizedBox(
-          height: 50,
-          child: TextField(
-            onChanged: (value) {},
-            decoration: InputDecoration(
+      margin: const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+      height: 50,
+      decoration: BoxDecoration(
+        color: Colors.grey.withOpacity(0.2),
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Row(
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 13.0),
+            child: Icon(Icons.search),
+          ),
+          Expanded(
+            child: TextField(
+              textAlign: TextAlign.left,
+              textCapitalization: TextCapitalization.sentences,
+              keyboardType: TextInputType.multiline,
+              decoration: InputDecoration(
                 hintStyle: TextStyle(color: Colors.grey[500], fontSize: 18),
                 hintText: translate(context, 'search_text'),
-                fillColor: Colors.grey.withOpacity(0.2),
-                filled: true,
-                prefixIcon: const Icon(
-                  // отцентрировать Иконку
-                  Icons.search,
-                ),
-                border: const OutlineInputBorder(
-                  borderSide: BorderSide(
-                    width: 0,
-                    style: BorderStyle.none,
-                  ),
-                  borderRadius: BorderRadius.all(Radius.circular(16)),
-                )),
-          )),
+                border: InputBorder.none,
+                contentPadding: const EdgeInsets.only(bottom: 12),
+              ),
+            ),
+          ),
+          const SizedBox(width: 15),
+        ],
+      ),
     );
   }
 }
