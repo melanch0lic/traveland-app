ce78aff1d3144410dd0733de3c3dc57be28ebba1
melanch0lic <kudza76@mail.ru>
2022-12-01 20:17:03 +0300

    hotels page change, webview import

--- hackground_app/lib/pages/hotels_page/hotels_page.dart
+++ hackground_app/lib/pages/hotels_page/hotels_page.dart
@@ -1,24 +1,52 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-import '../../widgets/search_widget.dart';
-import '../../widgets/filter_header.dart';
-import '../../widgets/appbar.dart';
-import 'components/attraction_listview_house_rent.dart';
-import '../../dummy_data.dart';
-
-class HotelsPage extends StatelessWidget {
+class HotelsPage extends StatefulWidget {
   const HotelsPage({Key? key}) : super(key: key);
+
+  @override
+  State<HotelsPage> createState() => _HotelsPageState();
+}
+
+class _HotelsPageState extends State<HotelsPage> {
+  late WebViewController? _webController;
+
+  @override
+  void initState() {
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _webController = null;
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // body: SafeArea(
+      //   child: Column(
+      //     children: [
+      //       const CustomAppBar(),
+      //       const SearchWidget(),
+      //       const FilterHeader('Жильё'),
+      //       AttractionListViewHouse(attractionList1),
+      //     ],
+      //   ),
+      // ),
       body: SafeArea(
-        child: Column(
-          children: [
-            const CustomAppBar(),
-            const SearchWidget(),
-            const FilterHeader('Жильё'),
-            AttractionListViewHouse(attractionList1),
-          ],
+        child: WebView(
+          javascriptMode: JavascriptMode.unrestricted,
+          initialUrl: 'https://sutochno.tp.st/EYdXrV3r',
+          onProgress: ((progress) {
+            print('WebView is loading (progress : $progress%)');
+          }),
+          gestureNavigationEnabled: true,
+          backgroundColor: const Color(0x00000000),
         ),
       ),
     );
